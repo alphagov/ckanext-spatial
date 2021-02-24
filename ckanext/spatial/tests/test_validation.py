5e00abbbdaf94a6edab6441192488ff6cf1614bc
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2021-02-24 17:46:21 +0000

    Change errs to errors in test_validation

--- ckanext/spatial/tests/test_validation.py
+++ ckanext/spatial/tests/test_validation.py
@@ -74,19 +74,16 @@ class TestValidation(object):
         assert len(errors) > 0
         assert_in("Descriptive keywords are mandatory", errors)
 
-    def assert_passes_all_gemini2_1_validation(self, xml_filepath):
-        errs = self.get_validation_errors(
-            validation.ISO19139EdenSchema, xml_filepath
-        )
-        assert not errs, "ISO19139EdenSchema: " + errs
-        errs = self.get_validation_errors(
-            validation.ConstraintsSchematron14, xml_filepath
-        )
-        assert not errs, "ConstraintsSchematron14: " + errs
-        errs = self.get_validation_errors(
-            validation.Gemini2Schematron, xml_filepath
-        )
-        assert not errs, "Gemini2Schematron: " + errs
+    def assert_passes_all_gemini2_validation(self, xml_filepath, gemini_schematron=validation.Gemini2Schematron):
+        errors = self.get_validation_errors(validation.ISO19139EdenSchema,
+                                          xml_filepath)
+        assert not errors, 'ISO19139EdenSchema: ' + errors
+        errors = self.get_validation_errors(validation.ConstraintsSchematron14,
+                                          xml_filepath)
+        assert not errors, 'ConstraintsSchematron14: ' + errors
+        errors = self.get_validation_errors(gemini_schematron,
+                                          xml_filepath)
+        assert not errors, 'Gemini2Schematron: ' + errors
 
     def test_04_dataset_valid(self):
         self.assert_passes_all_gemini2_1_validation(
